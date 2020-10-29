e4dc2584506d9d00029f026c616208390b39bf89
Revantark <revantark@gmail.com>
2020-10-29 10:19:41 +0530

    cleanUp

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-
-var openList = new Array();
+var openList = [];
 var closedlist = [];
 let rows;
 let cols;
@@ -12,12 +11,12 @@ let blockColor;
 let openListColor;
 let selectStart = true;
 let selectEnd = false;
-let selectBlockPath = false;
 let endPath = [];
 let end;
 let noSol = false;
 let start;
-let insructions;  
+let instructions;  
+
 function setup() {
   bgColor = color("#252a34");
   pathColor = color('#ee6f57');
@@ -35,10 +34,9 @@ function setup() {
     }
   }
   instructions = createP('Select a starting point'); 
-  instructions.position(windowWidth-350,0,);   
-  instructions.style('font-size', '32px');
+  instructions.position(windowWidth-350,0,); 
+  instructions.style('font-size', '28px');
   instructions.style('color', '#eaeaea');
-
   noLoop();
 }
 function windowResized() {
@@ -60,30 +58,9 @@ function keyPressed() {
 
 function draw() {
   instructions.show();
-  if (drawBlocks) {
-    var x = Math.floor(mouseX / side);
-    var y = Math.floor(mouseY / side);
-    let p = new Point(x, y);
-    if (x <= cols - 1 && y <= rows - 1){
-      let pp = blockedPath.find((ele) => {
-        return p.x == ele.x && p.y == ele.y;
-      });
-      let isStart = (x==start.x && y==start.y);
-      let isEnd = (x==end.x && y==end.y);
+  drawObstacles();
 
-      if (pp==undefined && p!=end && !isStart && !isEnd)
-        blockedPath.push(p);
-    }
-      
-  }
-
-  for (let block of blockedPath) {
-    fill(blockColor);
-    noStroke();
-    rect(block.x * side, block.y * side, side, side);
-  }
-
-
+  
   if (openList.length > 0) {
     var curr = removeHighestPriority();
     closedlist.push(curr);
@@ -97,25 +74,25 @@ function draw() {
     }
     
       let neighbours = curr.addNeighbours();
-      for (let i = 0; i < neighbours.length; i++) {
+      for (let neighbour of neighbours) {
         let cp = closedlist.find((p) => {
-          return neighbours[i].x == p.x && neighbours[i].y == p.y;
+          return neighbour.x == p.x && neighbour.y == p.y;
         });
         
         if (cp==undefined) {
           let tg = curr.g + 1;
-          if (tg < neighbours[i].g) {
-            neighbours[i].previous = curr;
-            neighbours[i].g = tg;
-            neighbours[i].h = abs(neighbours[i].x - end.x) + abs(neighbours[i].y - end.y);
-            neighbours[i].f = neighbours[i].h + neighbours[i].g;
+          if (tg < neighbour.g) {
+            neighbour.previous = curr;
+            neighbour.g = tg;
+            neighbour.h = abs(neighbour.x - end.x) + abs(neighbour.y - end.y);
+            neighbour.f = neighbour.h + neighbour.g;
 
           }
           let op = openList.find((p) => {
-            return neighbours[i].x == p.x && neighbours[i].y == p.y;
+            return neighbour.x == p.x && neighbour.y == p.y;
           }); 
           if (op == undefined) {
-            openList.push(neighbours[i]);
+            openList.push(neighbour);
           }
         }
 
@@ -155,23 +132,40 @@ function draw() {
     fill(color('#90DF7B'));
     noStroke();
     square(start.x * side, start.y * side, side);
-
   }
 
   if (!selectStart && !selectEnd) {
     fill(color('#506CF7'));
     noStroke();
-
     square(end.x * side, end.y * side, side);
   }
  
 
-  
 
-  //text('Select a start point', floor(windowWidth/2), 30      ,);
+}
 
+function drawObstacles(){
+  if (drawBlocks) {
+    var x = Math.floor(mouseX / side);
+    var y = Math.floor(mouseY / side);
+    let p = new Point(x, y);
+    if (x <= cols - 1 && y <= rows - 1){
+      let pp = blockedPath.find((ele) => {
+        return p.x == ele.x && p.y == ele.y;
+      });
+      let isStart = (x==start.x && y==start.y);
+      let isEnd = (x==end.x && y==end.y);
 
-
+      if (pp==undefined && p!=end && !isStart && !isEnd)
+        blockedPath.push(p);
+    }
+      
+  }
+  for (let block of blockedPath) {
+    fill(blockColor);
+    noStroke();
+    rect(block.x * side, block.y * side, side, side);
+  }
 }
 
 function mousePressed() {
@@ -206,6 +200,7 @@ function mousePressed() {
       selectBlockPath = true;
       
       instructions.html('Click and draw obstacles and Press S to start');
+    
       redraw();
       fill(color('#506CF7'));
       noStroke();
